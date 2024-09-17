fdc60687651290542dc09a5417d1a601bc8a5f68
quangnle <mnest2409@yahoo.com>
2024-09-17 15:49:21 +0700

    update logic

--- fireworks/fireworks.js
+++ fireworks/fireworks.js
@@ -2,11 +2,9 @@ class Fireworks {
     constructor(nParticles) {
         this.nParticles = nParticles;
         this.particles = [];
-        this.exploded = false;
     }
 
-    explode(x ,y) {
-        this.exploded = true;
+    explode(x, y) {
         for (let i = 0; i < this.nParticles; i++) {
             let p = new Particle(x, y, color(random(255), random(255), random(255)), random(2, 4));
             p.addForce(createVector(random(-1.0, 1.0), random(-1.0, 1.0)));
@@ -14,19 +12,23 @@ class Fireworks {
         }
     }
 
-    draw() {
-        if (this.exploded) {
-            for (let i = 0; i < this.particles.length; i++) {
-                //add gravity
-                this.particles[i].addForce(createVector(0, 0.03));
+    update() {
+        for (let i = 0; i < this.particles.length; i++) {
+            //add gravity
+            this.particles[i].addForce(createVector(0, 0.03));
+            // update and check if particle is dead
+            this.particles[i].update();
+            if (this.particles[i].life >= this.particles[i].maxLife) {
+                this.particles.splice(i, 1);
+                i--;
+            }         
+        }
+    }
 
-                this.particles[i].update();                
-                this.particles[i].draw();
-                if (this.particles[i].life >= this.particles[i].maxLife) {
-                    this.particles.splice(i, 1);
-                    i--;
-                }
-            }
+    draw() {
+        this.update();
+        for (let i = 0; i < this.particles.length; i++) {
+            this.particles[i].draw();
         }
     }
 }
