b2041c9a568e96da1980c82a0c450768f1743401
quangnle <mnest2409@yahoo.com>
2024-09-17 18:38:01 +0700

    update gauss distribution

--- gauss-distribution-formula/sketch.js
+++ gauss-distribution-formula/sketch.js
@@ -12,11 +12,9 @@ function drawOrigin(minX, maxX, minY, maxY) {
     // text(maxX, width - 20, height / 2 + 15);
     // text(minY, width / 2 + 5, height - 5);
     // text(maxY, width / 2 + 5, 15);
-
-    text("0", width / 2 + 5, height / 2 + 15);
 }
 
-function drawGaussian(mean, sigma, scaleX = 1, scaleY = 1) {
+function drawGaussian(mu, sigma, scaleX = 1, scaleY = 1) {
     stroke("#0f0");
     noFill();
     beginShape();
@@ -27,7 +25,7 @@ function drawGaussian(mean, sigma, scaleX = 1, scaleY = 1) {
     const step = 0.01;
 
     for (let x = startX; x < endX; x += step) {
-        let y = gaussian(x, mean, sigma);
+        let y = gaussian(x, mu, sigma);
         vertex(map(x, startX, endX, 0, width), map(y, startY, endY, height, 0));
     }
     endShape();
@@ -36,26 +34,41 @@ function drawGaussian(mean, sigma, scaleX = 1, scaleY = 1) {
     stroke("#00f");
     fill(color(0,0,255,100));
     beginShape();
-    for (let x = -sigma; x <= sigma; x += step) {
-        let y = gaussian(x, mean, sigma);
+    for (let x = mu-sigma; x <= mu+sigma+step; x += step) {
+        let y = gaussian(x, mu, sigma);
         vertex(map(x, startX, endX, 0, width), map(y, startY, endY, height, 0));
     }
-    vertex(map(sigma, startX, endX, 0, width), map(0, startY, endY, height, 0));
-    vertex(map(-sigma, startX, endX, 0, width), map(0, startY, endY, height, 0));    
+    vertex(map(mu + sigma, startX, endX, 0, width), map(0, startY, endY, height, 0));
+    vertex(map(mu - sigma, startX, endX, 0, width), map(0, startY, endY, height, 0));    
     endShape(CLOSE);    
+
+    // draw point at mu
+    strokeWeight(4);
+    stroke("#f00");
+    point(map(mu, startX, endX, 0, width), map(0, startY, endY, height, 0));
+    strokeWeight(1);
+    // draw line at mu
+    stroke("#f00");
+    line(map(mu, startX, endX, 0, width), 0, map(mu, startX, endX, 0, width), height);
+    // draw text at mu
+    stroke("#000");
+    fill("#000");
+    text("μ=" + mu, map(mu, startX, endX, 0, width), height / 2 + 15);
+    // draw text at sigma
+    fill("#000");
+    text("σ=" + (mu+sigma).toFixed(2), map(mu + sigma, startX, endX, 0, width), height / 2 + 15);
+    text("-σ=" + (mu-sigma).toFixed(2), map(mu - sigma, startX, endX, 0, width), height / 2 + 15);
+
 }
 
 function draw() {
     background(255);
-    push();
-    translate(0, height/2 - 20);
     drawOrigin(-5, 5, -2, 2);
     drawGaussian(mu, sigma, scaleX = 5, scaleY = 2);
-    pop();
 }
 
 function gaussian(x, mean, sigma) {
-    return (1 / (sigma*sqrt(2 * PI))) * exp(-0.5 * pow((x - mean) / sigma, 2));
+    return Math.exp(-0.5 * ((x - mean) / sigma) ** 2) / (sigma * Math.sqrt(2 * Math.PI));
 }
 
 /*
